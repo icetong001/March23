d4155b079de9706ca92e0336eb47e71d13223457
icetong001 <doralee1005@gmail.com>
2021-03-23 16:28:53 +0800

    commit  Customer

---
+++ src/main/java/cust/Customer.java
@@ -0,0 +1,13 @@
+package cust;
+
+public class Customer {
+    String id;
+    int amount;
+
+    public Customer (String id,int amount){
+        this.id=id;
+        this.amount=amount;
+
+    }
+    //public void
+}
